bf7c7360178d0ed34ee4b01b0339309731969f4d
talnoznisky <tal.noznisky@gmail.com>
2019-03-29 15:55:39 -0500

    Updates

---
+++ precache-manifest.764edd8dd720d27c6907a54b7caef2d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "76b6e97017c90e56b1ae",
+    "url": "/news-maker/static/css/main.3313a3bf.chunk.css"
+  },
+  {
+    "revision": "76b6e97017c90e56b1ae",
+    "url": "/news-maker/static/js/main.3df5e434.chunk.js"
+  },
+  {
+    "revision": "f33eb849d9f4aa3e2424",
+    "url": "/news-maker/static/js/runtime~main.e1cc0e1a.js"
+  },
+  {
+    "revision": "f49263851f42bb7ca3e8",
+    "url": "/news-maker/static/js/2.4d3076d5.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/news-maker/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "67e09f46b4c6c2887c611d4cec5da610",
+    "url": "/news-maker/index.html"
+  }
+];
